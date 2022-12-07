463f8f88aba2c641cf6f6b1f8bfa2c108790f719
joseph0017 <josephogiku96@gmail.com>
2022-12-07 12:32:28 -0800

    more data structures

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,8 +2,10 @@
 def weight_average(my_list=[]):
     numerator = 0
     denominator = 0
-
-    for numbers_in_tuples in my_list:
-        numerator += (numbers_in_tuples[0] * numbers_in_tuples[1])
-        denominator += (numbers_in_tuples[1])
-    return (numerator/ denominator)
+    
+    if my_list:
+        for numbers_in_tuples in my_list:
+            numerator += (numbers_in_tuples[0] * numbers_in_tuples[1])
+            denominator += (numbers_in_tuples[1])
+        return (numerator/ denominator)
+    return(0)
