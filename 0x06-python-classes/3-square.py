73e0af9b3e03f3ef61b8a2f7ab0a35977d382988
joseph0017 <66425797+joseph0017@users.noreply.github.com>
2022-12-20 04:22:43 -0800

    Update 3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -5,7 +5,7 @@
 class Square:
     """Class Square that defines a square"""
     def __init__(self, size=0):
-        if type (size) is not int:
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         elif (size < 0):
             raise ValueError("size must be >= 0")
