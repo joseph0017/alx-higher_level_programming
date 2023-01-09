6b21d72e472de9dba110715b85efc4ce13d1541b
joseph0017 <josephogiku96@gmail.com>
2023-01-09 05:31:03 -0800

    inheritance in python

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module for class BaseGeometry"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+    def __init__(self):
+        """a new instance of BaseGeometry"""
+        pass
+
+    def area(self):
+        """throw or raise exception error"""
+        raise Exception("area() is not implemented")
