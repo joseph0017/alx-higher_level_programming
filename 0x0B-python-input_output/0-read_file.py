49966264c53761f83b7c673c6f80a515b0afaab5
joseph0017 <josephogiku96@gmail.com>
2023-01-10 04:07:06 -0800

    fileIO

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module for read_file function"""
+
+
+def read_file(filename=""):
+    """
+    function that reads a text file (UTF8) and prints it to stdout
+    """
+    with open(filename, encoding="utf-8") as file:
+       open_read_file = file.read()
+       print(open_read_file)
+       file.close()
