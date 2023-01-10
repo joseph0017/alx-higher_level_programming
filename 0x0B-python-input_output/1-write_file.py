bd4934e150a3e97fd47a3767e52f66fbd1c905f2
joseph0017 <66425797+joseph0017@users.noreply.github.com>
2023-01-10 05:52:27 -0800

    Update 1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,6 +6,6 @@ def write_file(filename="", text=""):
     """Method that returns the number of lines of a text file"""
     with open(filename, "w+", encoding="utf-8") as myfile:
         myfile.write(text)
-        number_of_characters = len(text) + 5
+        number_of_characters = len(text)
         myfile.close()
         print(number_of_characters)
