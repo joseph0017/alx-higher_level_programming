c29c2a6f5f53439d7f9d356799564fe37ef01400
joseph0017 <66425797+joseph0017@users.noreply.github.com>
2023-01-10 04:14:54 -0800

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,6 +7,4 @@ def read_file(filename=""):
     function that reads a text file (UTF8) and prints it to stdout
     """
     with open(filename, encoding="utf-8") as my_file_0:
-       open_read_file = my_file_0.read()
-       print(open_read_file)
-       my_file_0.close()
+        print(my_file_0.read(), end="")
