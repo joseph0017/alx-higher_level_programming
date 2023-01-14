5955c61739fdc5828387216dd71ca1c2e6919a80
joseph0017 <josephogiku96@gmail.com>
2023-01-14 08:27:27 -0800

    tests

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module for Base class"""
+
+
+class Base:
+    """Base class for other classes in this module"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if (id is not None):
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
