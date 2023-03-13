209651afba49ce286d01dfb041dd33183856a042
joseph0017 <66425797+joseph0017@users.noreply.github.com>
2023-03-13 14:20:15 -0700

    Update 6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-  
-array.map((val) => {
-    console.log(`${val}`);
-});
+let i = 0;
+while (array[i]) {
+  console.log(array[i]);
+  i++;
+}
