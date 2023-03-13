46206144b59b6a7ceba21f00be8de81044e76d0f
joseph0017 <66425797+joseph0017@users.noreply.github.com>
2023-03-13 13:18:40 -0700

    Update 7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,8 +2,8 @@
 const args = process.argv[2];
 let i = 0;
 if (!args) {
-    console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 }
 for (i; i < args; i++) {
-    console.log('C is fun');
+  console.log('C is fun');
 }
