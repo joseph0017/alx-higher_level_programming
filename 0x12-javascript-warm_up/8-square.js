c23a0e596da4ae80fab1dc1d73c4d9ff7c569c4d
joseph0017 <66425797+joseph0017@users.noreply.github.com>
2023-03-13 13:21:04 -0700

    Update 8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,17 +1,14 @@
 #!/usr/bin/node
 const args = process.argv[2];
 const integer = parseInt(args);
-
 let stringX = '';
-
 if (!integer) {
-    console.log('Missing size')
-};
-
+  console.log('Missing size');
+}
 for (let i = 0; i < integer; i++) {
-    for(let j = 0; j < integer; j++) {
-        stringX += 'X';
-    };
-    stringX += '\n';
-};
+  for(let j = 0; j < integer; j++) {
+      stringX += 'X';
+  };
+  stringX += '\n';
+}
 console.log(stringX);
