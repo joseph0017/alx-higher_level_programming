2d66e8796a934d6910a55de88ccd46067b234520
joseph0017 <66425797+joseph0017@users.noreply.github.com>
2023-03-13 13:36:41 -0700

    Update 9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,10 +2,10 @@
 function add (a, b) {
   return a + b;
 }
-const args1 = process.argv[2];
-const parameter1 = parseInt(args1);
-const args2 = process.argv[3];
-const parameter2 = parseInt(args2);
-a = parameter1;
-b = parameter2;
+const argsOne = process.argv[2];
+const parameterOne = parseInt(argsOne);
+const argsTwo = process.argv[3];
+const parameterTwo = parseInt(argsTwo);
+a = parameterOne;
+b = parameterTwo;
 console.log(add(a, b));
