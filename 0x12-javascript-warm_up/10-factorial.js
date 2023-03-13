99ede316cb1bb604bfd6766e68acd60b3a60322d
joseph0017 <66425797+joseph0017@users.noreply.github.com>
2023-03-13 13:55:06 -0700

    testing

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 function factorial (x) {
   if (x == 0 || isNaN(x)) {
-      return 1;
+    return 1;
   } else {
-      return x * factorial(x - 1);
+    return x * factorial(x - 1);
   }
 }
 const x = parseInt(process.argv[2]);
