5240c804c8a7a292d7221743677f22ffc0c93523
joseph0017 <66425797+joseph0017@users.noreply.github.com>
2023-03-15 13:39:39 -0700

    Update 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -15,9 +15,10 @@ def add_integer(a, b=98):
     Returns:
         The sum of a and b
     """
-    if type(a) not in (int, float):
+    if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
-    if type(b) not in (int, float):
+    if not isinstance(b, int) and not isinstance(b, float):
         raise TypeError("b must be an integer")
-
-    return (int(a) + int(b))
+    a = int(a)
+    b = int(b)
+    return (a + b)
