5e556b95928e4bb85e1ae7f0af934df90bd9f3c5
joseph0017 <josephogiku96@gmail.com>
2023-03-17 04:48:24 -0700

    ORM

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     mycursor = connect_db.cursor()
     sql_query = """
 SELECT cities.name FROM cities
-JOIN states ON cities.state_id = cities.id
+JOIN states ON cities.state_id = states.id
 WHERE states.name = %s
 ORDER BY cities.id ASC
 """
