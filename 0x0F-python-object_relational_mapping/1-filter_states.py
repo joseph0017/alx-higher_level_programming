3ad6e196d30440c537c48340ccd0c8de1d4cf480
joseph0017 <66425797+joseph0017@users.noreply.github.com>
2023-03-16 23:51:01 -0700

    Update 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,13 +8,13 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                           passwd=argv[2], db=argv[3], charset="utf8")
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
-    query_rows = cur.fetchall()
+    connection = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                                passwd=argv[2], db=argv[3], charset="utf8")
+    mycursor = connection.cursor()
+    mycursor.execute("SELECT * FROM states WHERE states.name LIKE BINARY
+                           'N%' ORDER BY states.id ASC")
+    query_rows = mycursor.fetchall()
     for row in query_rows:
-        if row[1].startswith("N"):
-            print(row)
-    cur.close()
-    conn.close()
+        print(row)
+    mycursor.close()
+    connection.close()
