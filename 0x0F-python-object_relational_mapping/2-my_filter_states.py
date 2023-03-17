32e75702f4f224cd64313d00fd856e052b2c3d3e
joseph0017 <josephogiku96@gmail.com>
2023-03-16 23:18:55 -0700

    ORM

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -4,7 +4,7 @@
 import MySQLdb
 from sys import argv
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     connect_db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                                 passwd=argv[2], db=argv[3])
     mycursor = connect_db.cursor()
