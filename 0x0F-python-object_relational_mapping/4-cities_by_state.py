1b94b6862ae855124e35ca3cc882a5773105646f
joseph0017 <josephogiku96@gmail.com>
2023-03-17 04:14:54 -0700

    ORM

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
                                  passwd=argv[2], db=argv[3])
     mycursor = connect_db.cursor()
     sql_query = """
-SELECT * FROM cities
+SELECT cities.id, cities.name, states.name FROM cities
 JOIN states ON cities.id = states.id
 ORDER BY cities.id ASC
 """
