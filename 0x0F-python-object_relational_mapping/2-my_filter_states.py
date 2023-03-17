7b6a77cb06b6697cc02931b0e47e8a3c5a55cd39
joseph0017 <66425797+joseph0017@users.noreply.github.com>
2023-03-16 22:54:23 -0700

    Create 2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Filter states by user input"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ = "__main__":
+    connect_db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                                passwd=argv[2], db=argv[3])
+    mycursor = connect_db.cursor()
+    sql_query = "SELECT * FROM states WHERE name LIKE '{}' ORDER BY states.id ASC"
+    format_query = sql_query.format(argv[4])
+    mycursor.execute(format_query)
+    query_rows = mycursor.fetchall()
+    for results in query_rows:
+        print(results)
+    mycursor.close()
+    connect_db.close()
