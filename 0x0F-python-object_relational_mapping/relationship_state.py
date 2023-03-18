0375d2b52411d10ce35b12f4623c2cdfb66f06d6
joseph0017 <66425797+joseph0017@users.noreply.github.com>
2023-03-18 06:43:17 -0700

    Create relationship_state.py

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,23 @@
+
+#!/usr/bin/python3
+"""
+a python file that contains the class definition of a State
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    a blueprint for the states table
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False,
+                unique=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    
+    cities = relationship("City", backref="state")
