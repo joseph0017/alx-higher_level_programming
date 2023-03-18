44865e50da704d410cd676cd7114a300324ae459
joseph0017 <66425797+joseph0017@users.noreply.github.com>
2023-03-17 19:57:42 -0700

    Create 10-model_state_my_get.py

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,26 @@
+
+#!/usr/bin/python3
+"""
+prints the State object with the name passed
+as argument from the database hbtn_0e_6_usa
+"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine(
+      'mysql+mysqldb://{}:{}@localhost/{}'
+      .format(argv[1], argv[2], argv[3]),
+      pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    search_state = argv[4]
+    for state in session.query(State)\
+            .filter(State.name.contains(search_state)).all():
+        print(len(state))
+    session.close()
+    
