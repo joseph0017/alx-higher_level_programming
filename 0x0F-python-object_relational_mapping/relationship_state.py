b6985ebaf17c50ae5e7c10d016ce59cbb8e7ffb5
joseph0017 <66425797+joseph0017@users.noreply.github.com>
2023-03-18 07:20:11 -0700

    Update relationship_state.py

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -19,8 +19,4 @@ class State(Base):
                 unique=True, autoincrement=True)
     name = Column(String(128), nullable=False)
     
-    cities = relationship(
-        "City",
-        cascade="all, delete-orphan",
-        backref=backref("state", cascade="all"),
-        single_parent=True)
+    cities = relationship("City", backref="state")
