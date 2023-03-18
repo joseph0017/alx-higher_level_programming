5da417dd07b252dcd0cd194dc812cd795a3b897a
joseph0017 <josephogiku96@gmail.com>
2023-03-17 21:10:21 -0700

    ORM

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,6 +1,6 @@
-
 #!/usr/bin/python3
-"""changes the name of a state"""
+"""updates the name of a state"""
+
 from sys import argv
 from model_state import Base, State
 from sqlalchemy import (create_engine)
