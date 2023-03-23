6a95b9ced1f02d56251b588673f6063619509762
joseph0017 <josephogiku96@gmail.com>
2023-03-23 10:42:37 -0700

    removing whitespaces

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -4,46 +4,36 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """
-    class Square that inherits from the Rectangle class
-    """
+    """class Square that inherits from Base and Rectangle"""
     def __init__(self, size, x=0, y=0, id=None):
-        """class constructor"""
+        """Constructor"""
         super().__init__(size, size, x, y, id)
-        self.size = size
 
     @property
     def size(self):
-        """getter property for the size"""
+        """size of a rectangle"""
         return self.width
 
     @size.setter
     def size(self, value):
-        """getter property for the size value"""
+        """setter size"""
         self.width = value
         self.height = value
 
     def __str__(self):
-        """string representation for class Square"""
-        return(f"[{type(self).__name__}] ({self.id}) \
-                {self.x}/{self.y} - {self.size}")
+        """Prints string representation of a Square"""
+        return '[{}] ({}) {}/{} - {}'.\
+            format(type(self).__name__, self.id, self.x, self.y, self.size)
 
     def update(self, *args, **kwargs):
-        """
-        public method that assigns attributes:
-
-        *args is the list of arguments - no-keyworded arguments
-        1st argument should be the id attribute
-        2nd argument should be the size attribute
-        3rd argument should be the x attribute
-        4th argument should be the y attribute
-        """
+        """Public method: assigns an argument to each attribute"""
         attributes = ["id", "size", "x", "y"]
-        for attr, arg in zip(attributes, args):
-            setattr(self, attr, arg)
-        for attr, arg in kwargs.items():
-            setattr(self, attr, arg)
+        for atrr, arg in zip(attributes, args):
+            setattr(self, atrr, arg)
+        for atrr, arg in kwargs.items():
+            setattr(self, atrr, arg)
 
     def to_dictionary(self):
-        attributes = ["id", "x", "size", "y"]
+        """returns the dictionary representation of a Square"""
+        attributes = ["id", "size", "x", "y"]
         return {key: getattr(self, key) for key in attributes}
