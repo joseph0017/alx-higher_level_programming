afa876ff869876a36abee41ed8717e65067fce07
joseph0017 <josephogiku96@gmail.com>
2023-03-30 09:41:44 -0700

    python network

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!
-curl -so "0.0.0.0:5000/catch_me"
+curl -so -w "0.0.0.0:5000/catch_me"
