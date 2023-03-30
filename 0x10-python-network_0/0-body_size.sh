435efa182eabc44467e07813790e26c7202a7b8f
joseph0017 <66425797+joseph0017@users.noreply.github.com>
2023-03-30 07:31:34 -0700

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that sends a request to a URL and displays body size
-curl -so --write-out '%{size_download}\n' "$1"
+curl -so /dev/null --write-out '%{size_download}\n' "$1"
