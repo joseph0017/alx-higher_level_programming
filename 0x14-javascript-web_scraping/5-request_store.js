13bc14577eb0d4b9c2dde189dcc67289ffa8b89a
joseph0017 <josephogiku96@gmail.com>
2023-04-25 05:59:29 -0700

    web scraping

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const request = require('request');
+const fs = require('fs')
+const url = process.argv[2];
+const filename = process.argv[3];
+
+request(url, function (error, response, body) {
+  if (response) {
+    fs.writeFile(filename, body, 'utf-8', (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  } else {
+    console.log(error);
+  }
+});
