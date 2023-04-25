d087770e806302f1db3ae00be1ff81d07f911aad
joseph0017 <josephogiku96@gmail.com>
2023-04-25 05:20:18 -0700

    whatever

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile(process.argv[2], (err, data) => {
-  if (data) {
-    console.log(data);
-  } else {
-    console.log(err);
-  }
+
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
+  if (err) console.log(err);
+  else console.log(data);
 });
