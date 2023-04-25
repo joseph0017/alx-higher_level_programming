30ccf14db46275bcffb4938c438e9d6674391063
joseph0017 <josephogiku96@gmail.com>
2023-04-25 05:43:50 -0700

    web scraping

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,20 +5,11 @@ const arg = process.argv[2];
 let number = 0;
 
 request(arg, function (error, response, body) {
-<<<<<<< HEAD
-    if (response) {
-      data = JSON.parse(body)
-      for (const film of data.results) {
-          for(const character of film.characters) {
-              number += (character.endsWith(find_id) ? true : false);
-          }
-=======
   if (response) {
     const data = JSON.parse(body);
     for (const film of data.results) {
       for (const character of film.characters) {
         number += (character.endsWith(findId) ? 1 : true);
->>>>>>> ea0d0383bdfdc7d7ff0083b133cf3d540698ba07
       }
     }
     console.log(number);
