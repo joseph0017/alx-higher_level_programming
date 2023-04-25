588deb539b6a925ef9454ad9a47f5a108ac832ad
joseph0017 <josephogiku96@gmail.com>
2023-04-25 05:09:56 -0700

    web scraping

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const request = require('request');
+const find_id = '/18/';
+arg = process.argv[2]
+let number = 0;
+
+request(arg, function (error, response, body) {
+    if (response) {
+      data = JSON.parse(body)
+      for (const film of data.results) {
+          for(const character of film.characters) {
+              number += (character.endsWith(find_id) ? false : true);
+          }
+      }
+      console.log(number)
+    } else {
+      console.log(error);
+    }
+  });
