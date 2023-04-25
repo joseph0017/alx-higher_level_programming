e2ab05974142b6fb4ec77ccd0359f1a5c2d6cb4a
joseph0017 <josephogiku96@gmail.com>
2023-04-25 07:18:10 -0700

    commie

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,22 +3,20 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, function (error, response, body) {
-    if (response) {
-      const get_results = JSON.parse(body)
-      const userTaskCompleted = {};
-      get_results.map((result) => {
-          if (result.completed == true) {
-              if (result.userId in userTaskCompleted) {
-                  userTaskCompleted[result.userId] += 1;
-              }
-              else {
-                  userTaskCompleted[result.userId] = 1;
-              }
-          }
-      })
-      console.log(userTaskCompleted);
-    } else {
-      console.log(error);
-    }
-  });
-  
+  if (response) {
+    const getResults = JSON.parse(body);
+    const userTaskCompleted = {};
+    getResults.forEach((result) => {
+      if (result.completed === true) {
+        if (result.userId in userTaskCompleted) {
+          userTaskCompleted[result.userId] += 1;
+        } else {
+          userTaskCompleted[result.userId] = 1;
+        }
+      }
+    });
+    console.log(userTaskCompleted);
+  } else {
+    console.log(error);
+  }
+});
